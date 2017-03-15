8ea8b7150b618705b8f8d25142da22cc8da84eb3
Patrick Shin <pmjshin@gmail.com>
2017-03-14 22:00:53 -0400

    new impl of parsing and replacing text
    Cha

--- pythonParse.py
+++ pythonParse.py
@@ -2,14 +2,20 @@ import os
 import sys
 import re
 import fileinput
-path = "E:\\User\\Documents\\GitHub\\pythonParse"
+import ctypes  # An included library with Python install.
+
+path = "E:\\User\\Documents\\GitHub\\pythonBasics"
 regex = re.compile("(Sikorsky Aircrafts) (\d{4})")
 def parseAndReplace(filePath):
-    textFile = open(filePath, 'r')
-    for line in textFile:
-        if (regex.search(line)):
-            a = re.sub("(Sikorsky Aircrafts) (\d{4})", 'Lockheed Martin', line)
-            print(line, ' ', a)
+    #textFile = open(filePath)
+    #outFile = open(filePath, 'w')
+    with fileinput.input(filePath, inplace=True, backup= '.bak') as textFile:
+        for line in textFile:
+            ctypes.windll.user32.MessageBoxW(0, line, "Your title", 1)
+            line = re.sub("(Sikorsky Aircrafts) (\d{4})", 'Lockheed Martin 2017', line.rstrip())
+            ctypes.windll.user32.MessageBoxW(0, line, "Your title", 1)
+            print (line)
+            ##line.replace()
 
 for root, dirs, files in os.walk(path):
     path = root.split(os.sep)
